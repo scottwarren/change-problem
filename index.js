ba8bbbf7a476c2ae4500a8559aad90b032874a1a
Scott Warren <realscottwarren@gmail.com>
2018-07-26 04:40:58 +0300

    Clean up after taking a look at the code post-interview

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ const COINS = {
 // Think about when a currency doesn't have a 1 cent coin
 
 const solution = (amount, countryCode = 'US') => {
-    // if(!COINS[countryCode])
     if (COINS.hasOwnProperty(countryCode) === false) throw new Error(`Missing coin definition for "${countryCode}"`)
 
     const coinDemoninations = COINS[countryCode]
 
-    // console.log(coinDemoninations)
-
-    // Check if the amount is bigger than the biggest coin
     let returnValue = {}, remainingAmount = amount
 
     for (let index = 0; index < coinDemoninations.length; index++) {
         const coin = coinDemoninations[index]
-
-        // console.log({ coin })
-        // console.log({ remainingAmount })
         
         if (remainingAmount < coin) {
             returnValue[coin] = 0
@@ -28,15 +21,14 @@ const solution = (amount, countryCode = 'US') => {
         }
 
         const numberOfCoins = remainingAmount / coin
-        // console.log({ numberOfCoins })
         const amountOfCurrentCoin = Math.floor(numberOfCoins)
-        // console.log({ amountOfCurrentCoin })
+
         remainingAmount -= amountOfCurrentCoin * coin
 
         returnValue[coin] = amountOfCurrentCoin
     }
 
-    // Check for remainingAmount > 0 here to guard against remainder of amount
+    // Check for remainingAmount > 0 here to guard against ignoring the remainder of amount
 
     return returnValue
 }
